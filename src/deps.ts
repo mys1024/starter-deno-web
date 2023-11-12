5b8c74f240a287f5ac1b13f12fcccb14e7aba722
mys1024 <mys.1024@outlook.com>
2023-11-13 01:26:31 +0800

    chore: update deps

--- src/deps.ts
+++ src/deps.ts
@@ -1,15 +1,15 @@
 /* -------------------------------------------------- std -------------------------------------------------- */
 
-export { load as loadDotEnv } from "https://deno.land/std@0.196.0/dotenv/mod.ts";
+export { load as loadDotEnv } from "https://deno.land/std@0.206.0/dotenv/mod.ts";
 
-export * as base64 from "https://deno.land/std@0.196.0/encoding/base64.ts";
+export * as base64 from "https://deno.land/std@0.206.0/encoding/base64.ts";
 
-export * as colors from "https://deno.land/std@0.196.0/fmt/colors.ts";
+export * as colors from "https://deno.land/std@0.206.0/fmt/colors.ts";
 
 /* -------------------------------------------------- third party -------------------------------------------------- */
 
-export * as oak from "https://deno.land/x/oak@v12.6.0/mod.ts";
+export * as oak from "https://deno.land/x/oak@v12.6.1/mod.ts";
 
 export { oakCors } from "https://deno.land/x/cors@v1.2.2/mod.ts";
 
-export * as djwt from "https://deno.land/x/djwt@v2.9.1/mod.ts";
+export * as djwt from "https://deno.land/x/djwt@v3.0.1/mod.ts";
